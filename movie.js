e90664bc219ec8deae297d657ca6ecfc28dba08d
PrudhviBReddy <prudhvireddybokka1396@gmail.com>
2024-08-30 18:54:32 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,10 @@
+let movieName = "Salaar: Part 1 - Ceasefire";
+let movieDirector = "Prashanth Neel";
+let movieCast = ["Prabhas", "PruthviRaj Sukumaran"];
+
+
+let aboutSalaar = ()=>{
+    console.log("Salaar is a high-voltage action drama film written and directed by Prashanth Neel and produced by Vijay Kiragandur under Hombale Films.");
+};
+
+module.exports = {movieName,movieDirector,movieCast,aboutSalaar};
---
+++ server.js
@@ -0,0 +1,7 @@
+let {movieName,movieDirector,movieCast,aboutSalaar} = require("./movie.js");
+
+console.log(movieName);
+console.log(movieDirector);
+console.log(movieCast);
+aboutSalaar();
+
